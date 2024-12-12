cbef2f2ba9e8458a32f4bf3668a119f50c3d0084
Carlos Marti <carlos.marti29@yahoo.com>
2024-12-12 17:03:49 +0000

    Completed Lab

--- src/main/java/ContainsDuplicates.java
+++ src/main/java/ContainsDuplicates.java
@@ -1,4 +1,5 @@
 
+import java.util.HashSet;
 
 public class ContainsDuplicates {
     /**
@@ -8,6 +9,13 @@ public class ContainsDuplicates {
      * @return true if nums contains any duplicate values, false if it does not.
      */
     public boolean containsDuplicate(int[] nums){
+        
+        HashSet<Integer> numSet = new HashSet<Integer>();
+        for(int num : nums){
+            if(!numSet.add(num))
+                return true;
+        }
+
         return false;
     }
 }
